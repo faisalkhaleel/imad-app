fbe23743521da4d94e66fc1665d6b46fc357eed6
faisalkhaleel <30633438+faisalkhaleel@users.noreply.github.com>
2017-08-07 21:45:29 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,19 +78,16 @@ var htmlTemplate= `<html>
 `;
 return htmlTemplate;
 }
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 var counter = 0;
-app.get('/counter',function(req,res){
+app.get('/counter', function(req,res) {
     counter = counter + 1;
     res.send(counter.toString());
 });
-
-
-
-
 
 app.get('/:articleName', function (req, res) {
     //articleName==article-one
