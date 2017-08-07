e585dc36ff4c8ecf8205c1279f5e7b31f46893b5
faisalkhaleel <30633438+faisalkhaleel@users.noreply.github.com>
2017-08-07 21:14:12 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,25 @@
-console.log('Loaded!');
+//Counter code
+var button =document.getElementbyId('counter');
+counter = 0
+button.onclick=function () {
+    
+    // Create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture the variable in the correct span
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    // Not done yet
+    
+};
+// Make the request
+request.open('GET','http://faisalkhaleel.imad.hasura-app.io/counter',TRUE);
+request.send(null);
+};
