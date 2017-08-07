b2051c91832ae505ccb18afb730221851651f626
faisalkhaleel <30633438+faisalkhaleel@users.noreply.github.com>
2017-08-07 11:59:07 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/;articleName', function (req, res) {
+app.get('/:articleName', function (req, res) {
     //articleName==article-one
     //articles[articlName] --{} Object content for article
     var articleName = req.params.articleName;
