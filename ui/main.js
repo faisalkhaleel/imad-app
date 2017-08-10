9b3b6765409d155bfc00b33a84a2b96a2756f9e9
faisalkhaleel <30633438+faisalkhaleel@users.noreply.github.com>
2017-08-10 09:24:48 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,16 @@
+var button = document.getElementById('counter');
+var counter = 0;
+
+button.onclick = function () {
+    
+    counter= counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+};
+
+
+
+`
 // Counter code
 var button = document.getElementById('counter');
 
@@ -38,4 +51,4 @@ button.onclick = function () {
         }
         var ul = document.getElementById('namelist');
         ul.innerHTML = list;
-    };
+    };`
